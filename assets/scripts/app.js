d62a69e6211af13ea4daae8d2d9679b7e7f940f9
Venkatesh <venkatesh9ui@gmail.com>
2021-10-23 22:37:35 -0400

    Sending Data with a POST Request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((reslove, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -14,7 +14,7 @@ function sendHttpRequest(method, url) {
       //   console.log(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
   return promise;
 }
@@ -34,4 +34,16 @@ async function fetchPosts() {
     listElement.append(postEl);
   }
 }
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+}
+
 fetchPosts();
+createPost('DUMMY', 'A Dummy post!');
