1860a861f5988e05a88efe3c51af1909e1c1b17d
Venkatesh <venkatesh9ui@gmail.com>
2021-10-24 00:12:45 -0400

    Sending a DELETE Request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((reslove, reject) => {
@@ -33,6 +34,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
     listElement.append(postEl);
   }
 }
@@ -58,6 +60,16 @@ form.addEventListener('submit', (event) => {
   const eneteredContent = event.currentTarget.querySelector('#content').value;
 
   createPost(enteredTitle, eneteredContent);
+});
+
+postList.addEventListener('click', (event) => {
+  if ((event.target.tagName = 'BUTTON')) {
+    const postId = event.target.closest('li').id;
+    sendHttpRequest(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
 });
 
 // fetchPosts();
