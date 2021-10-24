ccb76b54ebfb21278fea9d9d969c9a7513a197f5
Venkatesh <venkatesh9ui@gmail.com>
2021-10-23 21:40:33 -0400

    Promisifying Http Requests (with XMLHttpRequest) - 1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,37 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((reslove, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.open(method, url);
 
-xhr.responseType = 'json'; // One way of parsing the JSON data
+    xhr.responseType = 'json'; // One way of parsing the JSON data
 
-xhr.onload = function () {
-  //   console.log(xhr.response);
-  const listOfPosts = xhr.response; // If we're using xhr.responseType we don't need to write manually JSON.parse
-  // const listOfPosts = JSON.parse(xhr.response); // One other way of parsing the data (manually)
-  // console.log(listOfPosts);
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    listElement.append(postEl);
-  }
-};
+    xhr.onload = function () {
+      reslove(xhr.response);
+      //   console.log(xhr.response);
+    };
 
-xhr.send();
+    xhr.send();
+  });
+  return promise;
+}
+
+function fetchPosts() {
+  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+    (responseData) => {
+      const listOfPosts = responseData; //xhr.response; // If we're using xhr.responseType we don't need to write manually JSON.parse
+      // const listOfPosts = JSON.parse(xhr.response); // One other way of parsing the data (manually)
+      // console.log(listOfPosts);
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+      }
+    }
+  );
+}
+fetchPosts();
