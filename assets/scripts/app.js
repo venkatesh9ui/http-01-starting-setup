8c969578c2f8032f8a38976d98441f9657812752
Venkatesh <venkatesh9ui@gmail.com>
2021-10-24 00:49:45 -0400

    Using the fetch() API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,51 +5,52 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((reslove, reject) => {
-    const xhr = new XMLHttpRequest();
+  // const promise = new Promise((reslove, reject) => {
+  //   const xhr = new XMLHttpRequest();
 
-    xhr.open(method, url);
+  //   xhr.open(method, url);
 
-    xhr.responseType = 'json'; // One way of parsing the JSON data
+  //   xhr.responseType = 'json'; // One way of parsing the JSON data
 
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        reslove(xhr.response);
-      } else {
-        reject(new Error('Something went wrong!'));
-      }
+  //   xhr.onload = function () {
+  //     if (xhr.status >= 200 && xhr.status < 300) {
+  //       reslove(xhr.response);
+  //     } else {
+  //       reject(new Error('Something went wrong!'));
+  //     }
 
-      //   console.log(xhr.response);
-    };
+  //     //   console.log(xhr.response);
+  //   };
 
-    xhr.onerror = function () {
-      reject(new Error('Failed to send request!'));
-    };
+  //   xhr.onerror = function () {
+  //     reject(new Error('Failed to send request!'));
+  //   };
 
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
+  //   xhr.send(JSON.stringify(data));
+  // });
+  // return promise;
+  return fetch(url).then((response) => response.json());
 }
 
 async function fetchPosts() {
-  try {
-    const responseData = await sendHttpRequest(
-      'GET',
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    const listOfPosts = responseData; //xhr.response; // If we're using xhr.responseType we don't need to write manually JSON.parse
-    // const listOfPosts = JSON.parse(xhr.response); // One other way of parsing the data (manually)
-    // console.log(listOfPosts);
-    for (const post of listOfPosts) {
-      const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector('h2').textContent = post.title.toUpperCase();
-      postEl.querySelector('p').textContent = post.body;
-      postEl.querySelector('li').id = post.id;
-      listElement.append(postEl);
-    }
-  } catch (error) {
-    alert(error.message);
+  // try {
+  const responseData = await sendHttpRequest(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const listOfPosts = responseData; //xhr.response; // If we're using xhr.responseType we don't need to write manually JSON.parse
+  // const listOfPosts = JSON.parse(xhr.response); // One other way of parsing the data (manually)
+  // console.log(listOfPosts);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
+    listElement.append(postEl);
   }
+  // } catch (error) {
+  //   alert(error.message);
+  // }
 }
 
 async function createPost(title, content) {
